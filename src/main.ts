6e5964a5f1279cb47e2c437705766da908a14fbd
novel86 <143467869+click-point-dev@users.noreply.github.com>
2025-07-02 13:39:03 +0700

    install and sutup api documentation swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
 	const isDev = process.env.NODE_ENV === 'development';
 	const PORT = isDev ? 8641 : 80;
 	const app = await NestFactory.create(AppModule);
+
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle('Advanced backend lesson')
+		.setDescription('Api documentation')
+		.setVersion('1.0.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('/api/docs', app, document);
 
 	await app.listen(PORT, () => console.log(`server started on ${PORT} port`));
 }
